d6a02369137ee81218070975a0383df037108e24
Javi <javirod.cba@gmail.com>
2021-06-17 17:49:11 +1000

    Add functionality to prev and back buttons

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+// A Step is a Natural number [1,4]
+
+// Initial step
+let step = 1;
+const inc = (step) => (step < 4 ? step + 1 : step);
+const dec = (step) => (step > 1 ? step - 1 : step);
+
+const steps = document.querySelectorAll("li");
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+const updateSteps = () => {
+  steps.forEach((s, i) =>
+    i < step ? s.classList.add("completed") : s.classList.remove("completed")
+  );
+};
+
+next.addEventListener("click", () => {
+  step = inc(step);
+  updateSteps();
+});
+
+prev.addEventListener("click", () => {
+  step = dec(step);
+  updateSteps();
+});
